137de59c64dfc1625dba177a9bf3d8d3c755429a
komilM <komiljonmeliyev315@gmail.com>
2026-01-03 15:56:37 +0500

    navni menusi tugavotti

--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -4,16 +4,20 @@ import logo from "../assets/logo.png"
 
 const Nav = () => {
   return (
-    <div>
+    <div className='body'>
       <img src={logo} alt="" />
       <div className="nav-menu">
         <div className="lang-box">
-          <h1></h1>
+          <h1 className='lang-btn'>English</h1>
 
           <div className="lang-menu">
-            <h1></h1>
+            <h1>English</h1>
+            <h1>Russia</h1>
+            <h1>Uzbek</h1>
           </div>
         </div>
+        <h1>Search Food</h1>
+        <button></button>
       </div>
     </div>
   )
